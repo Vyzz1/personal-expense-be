129f91486d4ddb8c95441d96cc07bf8266958890
Huỳnh Khang Vỹ <vykhanghuynh@gmail.com>
2026-02-19 13:51:47 +0700

    basic CRUD for category domain

---
+++ src/main/java/com/huynh/personal_expense_be/modules/category/domain/Category.java
@@ -0,0 +1,4 @@
+package com.huynh.personal_expense_be.modules.category.domain;
+
+public class Category {
+}
---
+++ src/main/java/com/huynh/personal_expense_be/modules/category/infrastructure/persistence/CategoryJpaEntity.java
@@ -0,0 +1,4 @@
+package com.huynh.personal_expense_be.modules.category.infrastructure.persistence;
+
+public class CategoryJpaEntitiy {
+}
