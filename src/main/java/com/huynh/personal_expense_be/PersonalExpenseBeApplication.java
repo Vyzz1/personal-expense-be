2ecaf63c5e32b24adb54ed59e84d976117279f02
Huỳnh Khang Vỹ <vykhanghuynh@gmail.com>
2026-02-19 12:17:56 +0700

    first commit

---
+++ src/main/java/com/huynh/personal_expense_be/PersonalExpenseBeApplication.java
@@ -0,0 +1,13 @@
+package com.huynh.personal_expense_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpenseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExpenseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huynh/personal_expense_be/PersonalExpenseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huynh.personal_expense_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpenseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
