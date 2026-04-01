3deec12a3e52250112b7dac1d1e8af48cd038efd
Huỳnh Khang Vỹ <vykhanghuynh@gmail.com>
2026-04-01 15:57:11 +0700

    feat : add CORS configuration for handling cross-origin requests

--- src/main/java/com/huynh/personal_expense_be/shared/config/CorsConfig.java
+++ src/main/java/com/huynh/personal_expense_be/shared/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.huynh.personal_expense_be.config;
+package com.huynh.personal_expense_be.shared.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
